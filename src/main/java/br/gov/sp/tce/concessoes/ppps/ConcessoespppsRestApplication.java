04d8f9cb6d653da92e8a0ed88524900f7c675990
raffrz <ralves96@outlook.com>
2021-10-15 10:25:55 -0300

    Initial commit

---
+++ src/main/java/br/gov/sp/tce/concessoes/ppps/ConcessoespppsRestApplication.java
@@ -0,0 +1,11 @@
+package br.gov.sp.tce.concessoes.ppps;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class ConcessoespppsRestApplication extends Application {
+}
